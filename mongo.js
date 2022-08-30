f5432bc2e10f1f100298cd46bfef467a5c5e4f69
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-30 17:24:49 +0300

    Finish submission 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+if (process.argv[3] && process.argv[4]) {
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name + " " + person.number);
+    });
+    mongoose.connection.close();
+  });
+}
